687114e3a8cc943cee8278ef77cd0cb359058005
Patrick Pogscheba <patrick.pogscheba@hs-duesseldorf.de>
2023-03-15 12:42:53 +0100

    added volume & connection state to AppState

--- Runtime/models/AppState.cs
+++ Runtime/models/AppState.cs
@@ -13,12 +13,18 @@ namespace PuzzleCubes.Models
         [UnityEngine.SerializeField] private string cubeId;
         [UnityEngine.SerializeField] private bool isRunning;
 
+        [UnityEngine.SerializeField] private float volume;
+
+        [UnityEngine.SerializeField] private bool mqttConnected;
+
         public string AppName { get => appName; set => appName = value; }
         public string CubeId { get => cubeId; set => cubeId = value; }
         public bool IsRunning { get => isRunning; set => isRunning = value; }
 
+        public bool Volume { get => volume; set => volume = value; }
+        public bool MqttConnected { get => mqttConnected; set => mqttConnected = value; }
 
-        // TODO: Add Commuications states
+
     }
 
 }
